2ec1cc49203390e61d272521b4899952260317ea
aryzhkov <aryzhkov@aquivalabs.com>
2019-11-19 15:48:34 +0300

    extra log

--- lib/sfdc.sh
+++ lib/sfdc.sh
@@ -28,8 +28,7 @@ sfdx_delete_scratch() {
 }
 
 install_package_version() {
-  PACKAGE_NAME=$1
-  PACKAGE_VERSION_JSON="$(eval sfdx force:package:version:list -v $2 -p $PACKAGE_NAME --json | jq '.result | sort_by(-.MajorVersion, -.MinorVersion, -.PatchVersion, -.BuildNumber) | .[0] // ""')"
+  PACKAGE_VERSION_JSON="$(eval sfdx force:package:version:list -v $2 --json -p $1 | jq '.result | sort_by(-.MajorVersion, -.MinorVersion, -.PatchVersion, -.BuildNumber) | .[0] // ""')"
   echo $PACKAGE_VERSION_JSON
 
   IS_RELEASED=$(jq -r '.IsReleased?' <<< $PACKAGE_VERSION_JSON)
