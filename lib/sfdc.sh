bb76b6c5ec958685b5b3f923dab1faada4b90178
aryzhkov <aryzhkov@aquivalabs.com>
2019-11-23 22:45:59 +0300

    add logs

--- lib/sfdc.sh
+++ lib/sfdc.sh
@@ -109,9 +109,12 @@ validate_package() {
   PACKAGE_ON_DEVHUB=$(is_package_exists_on_devhub $1 $2)
   PACKAGE_IN_PROJECT_FILE=$(is_package_exists_in_project_file "$2")
   echo "$PACKAGE_ON_DEVHUB"
-  echo "$PACKAGE_IN_PROJECT_FILE"
+  echo "$PACKAGE_IN_PROJECT_FILE == false"
+  if [ "$PACKAGE_IN_PROJECT_FILE" == "false" ]; then
+  echo "122333"
+  fi
 
-  if [[ "$PACKAGE_ON_DEVHUB" = "false" || "$PACKAGE_IN_PROJECT_FILE" = "false" ]]; then
+  if [[ "$PACKAGE_ON_DEVHUB" == "false" || "$PACKAGE_IN_PROJECT_FILE" == "false" ]]; then
     echo "Please install your package in your Dev Hub and update sfdx-project.json file"
     exit 1
   fi
