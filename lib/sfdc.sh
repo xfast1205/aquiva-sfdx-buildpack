476e99decdfbc352f1a63898493fac73e6cd6669
aryzhkov <aryzhkov@aquivalabs.com>
2019-11-24 18:31:02 +0300

    add pwd

--- lib/sfdc.sh
+++ lib/sfdc.sh
@@ -109,6 +109,7 @@ prepare_proc() {
     echo "# Deploy source to prodyuction org.
     release: ./release/release.sh \"$1\" \"$2\" \"$3\" \"$4\" \"$5\"" > $5/Procfile
 
+    cd ..
     pwd
     ls -la
     cp /lib/release.sh $5/lib/
