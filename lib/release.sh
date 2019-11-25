6dca5e1fefe1d8af97adaac1498014a787a56b93
aryzhkov <aryzhkov@aquivalabs.com>
2019-11-25 13:45:22 +0300

    fix release

--- lib/release.sh
+++ lib/release.sh
@@ -8,8 +8,8 @@ set -o pipefail     # don't ignore exit codes when piping output
 
 SFDX_PACKAGE_NAME=${1:-}
 SFDX_PACKAGE_VERSION_ID=${2:-}
-ORG_USERNAME=${3:-}
-INSTANCE_URL=${4:-}
+TARGET_ORG_USERNAME=${3:-}
+TARGET_ORG_INSTANCE_URL=${4:-}
 DEVHUB_USERNAME=${5:-}
 DEVHUB_INSTANCE_URL=${6:-}
 BP_DIR="."
@@ -25,9 +25,9 @@ header "Running release.sh ..."
 promote_package() {
   log "Promote package ..."
 
-  # prepare_sfdc_environment \
-  #   "$DEVHUB_INSTANCE_URL" \
-  #   "$DEVHUB_USERNAME"
+  prepare_sfdc_environment \
+    "$DEVHUB_INSTANCE_URL" \
+    "$DEVHUB_USERNAME"
 
   sfdx force:package:version:promote \
     -p "$SFDX_PACKAGE_VERSION_ID" \
@@ -35,12 +35,12 @@ promote_package() {
     -n
 
   prepare_sfdc_environment \
-    "$INSTANCE_URL" \
-    "$ORG_USERNAME"
+    "$TARGET_ORG_INSTANCE_URL" \
+    "$TARGET_ORG_USERNAME"
 
   sfdx force:package:install \
     -p "$SFDX_PACKAGE_VERSION_ID" \
-    -u "$ORG_USERNAME" \
+    -u "$TARGET_ORG_USERNAME" \
     -w 10 \
     -b 10 \
     -r
