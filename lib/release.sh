c6624baafc77c76c14c6a5c80dbf9e9c253bfd27
aryzhkov <aryzhkov@aquivalabs.com>
2019-11-25 13:38:12 +0300

    remove log

--- lib/release.sh
+++ lib/release.sh
@@ -24,31 +24,30 @@ header "Running release.sh ..."
 
 promote_package() {
   log "Promote package ..."
-  INSTANCE_URL=${1:-}
 
-  prepare_sfdc_environment \
-    "$DEVHUB_INSTANCE_URL" \
-    "$DEVHUB_USERNAME"
+  # prepare_sfdc_environment \
+  #   "$DEVHUB_INSTANCE_URL" \
+  #   "$DEVHUB_USERNAME"
 
   sfdx force:package:version:promote \
     -p "$SFDX_PACKAGE_VERSION_ID" \
     -v "$DEVHUB_USERNAME" \
     -n
-  echo "$INSTANCE_URL"
+
   prepare_sfdc_environment \
     "$INSTANCE_URL" \
     "$ORG_USERNAME"
 
   sfdx force:package:install \
-  -p "$SFDX_PACKAGE_VERSION_ID" \
-  -u "$ORG_USERNAME" \
-  -w 10 \
-  -b 10 \
-  -r
+    -p "$SFDX_PACKAGE_VERSION_ID" \
+    -u "$ORG_USERNAME" \
+    -w 10 \
+    -b 10 \
+    -r
 
 }
 
-promote_package "$INSTANCE_URL"
+promote_package
 
 header "DONE! Completed in $(($SECONDS - $START_TIME))s"
 exit 0
