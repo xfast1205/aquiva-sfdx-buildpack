04415fdc344ab38de3c0f4100e1ad4014037a9d5
aryzhkov <aryzhkov@aquivalabs.com>
2019-11-26 13:03:39 +0300

    new logs

--- lib/sfdc.sh
+++ lib/sfdc.sh
@@ -110,7 +110,6 @@ check_package_in_project_file() {
     jq -r --arg PACKAGE_NAME "$PACKAGE_NAME" '.packageDirectories[]
       | select(.package==$PACKAGE_NAME)')"
       ls -la
-  echo "1$IS_PACKAGE_EXISTS"
 
   PACKAGE_ID="$(sfdx force:package:list -v "$USERNAME" --json |
       jq -r --arg PACKAGE_NAME "$PACKAGE_NAME" --arg PACKAGE_TYPE "$PACKAGE_TYPE" '.result[]
@@ -118,13 +117,11 @@ check_package_in_project_file() {
         | select(.ContainerOptions==$PACKAGE_TYPE)
         .Id')"
 
-    echo "2$PACKAGE_ID"
-
   if [ ! -z "$PACKAGE_ID" ]; then
+    echo "123"
     IS_CONTAINS_ID="$(grep "$PACKAGE_ID" "./sfdx-project.json")"
+    echo "&IS_CONTAINS_ID"
   fi
-
-  echo "3$IS_CONTAINS_ID"
 
   if [[ -z "$IS_PACKAGE_EXISTS" || -z "$IS_CONTAINS_ID" ]]; then
     PACKAGE_PATH="$(cat sfdx-project.json |
